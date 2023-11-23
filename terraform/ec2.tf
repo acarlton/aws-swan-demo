1fb02d4adc7b52083fe2c5d3a43a4d1fc9d4e6e8
Aaron Carlucci <resume@carlucci.aleeas.com>
2023-11-23 11:38:58 +0100

    Renamespace web security group to app and decouple security group rules to separate aws_security_group_rule resources

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,29 +1,29 @@
 # Security group for the ALB accepting HTTP connections on port 80
 #
 # TODO: implement SSL encrypted traffic and redirect HTTP to HTTPS
-resource "aws_security_group" "web" {
-  name   = "${local.namespace}-web"
+resource "aws_security_group" "alb" {
+  name   = "${local.namespace}-alb"
   vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_security_group_rule" "web_ingress_http" {
+resource "aws_security_group_rule" "alb_ingress_http" {
   cidr_blocks       = ["0.0.0.0/0"]
   description       = "Allow public HTTP traffic"
   from_port         = 80
   ipv6_cidr_blocks  = ["::/0"]
   protocol          = "tcp"
-  security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.alb.id
   to_port           = 80
   type              = "ingress"
 }
 
-resource "aws_security_group_rule" "web_egress_all" {
+resource "aws_security_group_rule" "alb_egress_all" {
   cidr_blocks       = ["0.0.0.0/0"]
   description       = "Allow all outbound traffic"
   from_port         = 0
   ipv6_cidr_blocks  = ["::/0"]
   protocol          = -1
-  security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.alb.id
   to_port           = 0
   type              = "egress"
 }
@@ -49,7 +49,7 @@ resource "aws_lb" "alb" {
   name               = local.namespace
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.web.id]
+  security_groups    = [aws_security_group.alb.id]
   subnets            = local.public_subnet_ids
 }
 
