f8421289bf4cfa96e9fb3ab14350188207aa95b2
Aaron Carlucci <resume@carlucci.aleeas.com>
2023-11-23 11:39:35 +0100

    Add public traffic egress rule to app security group

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -88,9 +88,20 @@ resource "aws_security_group_rule" "app_ingress_http" {
   from_port                = 80
   protocol                 = "tcp"
   security_group_id        = aws_security_group.app.id
-  source_security_group_id = aws_security_group.web.id
+  source_security_group_id = aws_security_group.alb.id
   to_port                  = 80
   type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "app_egress_all" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow all outbound traffic"
+  from_port         = 0
+  ipv6_cidr_blocks  = ["::/0"]
+  protocol          = -1
+  security_group_id = aws_security_group.app.id
+  to_port           = 0
+  type              = "egress"
 }
 
 # Hello World ECS service
